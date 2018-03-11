2aff54b5d3505b4ad8413656f0b69caffb7f27f6
Siddiqui Noor <siddiquinoor@users.noreply.github.com>
2018-03-11 12:27:08 +0600

    Update new_author.php

--- new_author.php
+++ new_author.php
@@ -8,9 +8,10 @@
 <body>
   <h1>New Author</h1>
   
-  <div>Author Name <input type="text" name="author_name"></div>
-  <div>Author Name <textarea rows="3" cols="50"></textarea></div>
-  <div><input type="submit" value="SAVE"><input type="reset" value="RESET"></div>
-  
+  <form action="" method="post">
+    <div>Author Name <input type="text" name="author_name"></div>
+    <div>Author Name <textarea rows="3" cols="50"></textarea></div>
+    <div><input type="submit" value="SAVE"><input type="reset" value="RESET"></div>
+  </form>
 </body>
 </html>
