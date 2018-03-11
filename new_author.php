e8446e2ed62f39859d8ec1545937aea1f43cb319
Siddiqui Noor <siddiquinoor@users.noreply.github.com>
2018-03-11 12:19:05 +0600

    Create new_author.php

---
+++ new_author.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>New Author</title>
+</head>
+
+<body>
+  <h1>New Author</h1>
+  
+  <div>Author Name <input type="text" name="author_name"></div>
+  <div>Author Name <textarea rows="3" cols="50"></textarea></div>
+  <div><input type="submit" value="SAVE"><input type="reset" value="RESET"></div>
+  
+</body>
+</html>
