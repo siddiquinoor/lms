56b40e784d45334926fe3d8be43c14bd4ef5f7fc
Siddiqui Noor <siddiquinoor@users.noreply.github.com>
2018-03-18 13:12:55 +0700

    Create add_author.php

---
+++ add_author.php
@@ -0,0 +1,14 @@
+<?php
+  if( isset($_POST) && $_POST['author_name'] != "" )
+  {
+    // connecting to the database
+    $conn = mysqli_connect("localhost", "root", "", "lms");
+    mysqli_query("INSERT INTO authors SET author_name='$author_name', author_bio='$author_bio'", $conn);
+    header("Loction: new_author.php?msg=Author added!");
+  }
+  else
+  {
+    die("No author name given");
+    echo "<a href="new_author.php">Go Back</a>";
+  }
+?>
